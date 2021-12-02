b779fd7cf46ecb4eb035a3f8a3ec94fea0259a06
Fidomivalua <sidonie.moulart@gmail.com>
2021-12-02 22:25:00 +0100

    Add server for deployment

---
+++ server.js
@@ -0,0 +1,32 @@
+// server.js
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    try {
+        createServer((req, res) => {
+            // Be sure to pass `true` as the second argument to `url.parse`.
+            // This tells it to parse the query portion of the URL.
+            const parsedUrl = parse(req.url, true);
+            const { pathname, query } = parsedUrl;
+
+            if (pathname === "/about") {
+                app.render(req, res, "/about", query);
+            } else if (pathname === "/projects") {
+                app.render(req, res, "/projects", query);
+            } else {
+                handle(req, res, parsedUrl);
+            }
+        }).listen(3000, (err) => {
+            if (err) throw err;
+            console.log("> Ready on http://localhost:3000");
+        });
+    } catch (error) {
+        console.error(error);
+    }
+});
