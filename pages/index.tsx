b4e6b5873bb34a947fa42da3ae002c9e58b6bde0
Fidomi <sidonie.moulart@gmail.com>
2021-12-03 15:51:44 +0100

    Remove console.log

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Layout } from "../components/Layout";
 import Map from "../components/Map/Map";
 export default function Home() {
     const curColor: string = "bg-yellow-400";
+    console.log(process.env);
     return (
         <Layout curColor={curColor}>
             <div className={`container px-4 mx-auto ${curColor}`}>
