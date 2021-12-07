f34983055c5e9f747a065d11469218ff3bd3e9f1
Fidomi <sidonie.moulart@gmail.com>
2021-12-07 11:28:21 +0100

    Add portfolio project

--- data/about.ts
+++ data/about.ts
@@ -10,9 +10,9 @@ type AboutType = {
 export const ABOUT_EN: AboutType = {
     salutation: "Hi there! ",
     title: "I'm Sidonie, front-end developer from France.",
-    desc: "I like to code things from scratch as much as using React or Next. I'm a quick learner and adapt really fast. I also have skills in graphic techniques since I spent more than 10 years in the VFX field (compositing and motion design) for TV series and long feature films.",
+    desc: "I like to code things from scratch as much as using React or Next. I also have skills in graphic techniques since I spent more than 10 years in the VFX field (compositing and motion design) for TV series and long feature films.",
     technologies: [
-        "Languages : TypeScript / JavaScript, HTML, CSS, Less, MySQL, Pug, Twig, Less",
+        "Languages : TypeScript / JavaScript, HTML, CSS, Less, MySQL, Pug, Twig",
         "Dev Tools : React, Next, Node, Express, MongoDB, Gulp, Git, Bootstrap, Tailwind",
         "Design Tools : Figma, Photoshop, Illustrator",
         "VFX Tools : Nuke, After Effects, Unreal Engine 4 (lighting/shading), Blender (basics)",
@@ -24,7 +24,7 @@ export const ABOUT_EN: AboutType = {
 export const ABOUT_FR: AboutType = {
     salutation: "Bonjour! ",
     title: "Je suis Sidonie, développeuse front-end.",
-    desc: "J'aime coder sans framework tout autant qu'en utilisant React et Next. J'apprends vite et m'adapte facilement. Après 10 années passées dans les effets spéciaux numériques pour le cinéma, j'ai aussi de bonnes compétences graphiques (compositing et motion design).",
+    desc: "J'aime coder sans framework tout autant qu'avec React et Next. Après 10 années passées dans les effets spéciaux numériques pour le cinéma, j'ai aussi de bonnes compétences graphiques (compositing et motion design).",
     technologies: [
         "Langages : TypeScript / JavaScript, HTML, CSS, Less, MySQL, Pug, Twig, Less",
         "Outils de développement : React, Next, Node, Express, MongoDB, Gulp, Git, Bootstrap, Tailwind",
--- data/projects.ts
+++ data/projects.ts
@@ -1,9 +1,9 @@
-import demo2018 from "../public/img/demo_2018_Y.png";
-import demo2016 from "../public/img/demo_2016_Y.png";
-import gardenMap from "../public/img/my_garden_map_Y.png";
-import seat from "../public/img/seat_Y.png";
-import amap from "../public/img/amap_Y.png";
-import walkcycle from "../public/img/walk_cycle_Y.png";
+import demo2018_O from "../public/img/demo_2018_O.png";
+import demo2016_O from "../public/img/demo_2016_O.png";
+import gardenMap_O from "../public/img/my_garden_map_O.png";
+import seat_O from "../public/img/seat_O.png";
+import amap_O from "../public/img/amap_O.png";
+import walkcycle_O from "../public/img/walk_cycle_O.png";
 import demo2018_16_9 from "../public/img/demo_2018_C.png";
 import demo2016_16_9 from "../public/img/demo_2016_C.png";
 import gardenMap_16_9 from "../public/img/my_garden_map_C.png";
@@ -13,15 +13,31 @@ import walkcycle_16_9 from "../public/img/walk_cycle_C.png";
 import ailleurs_1 from "../public/img/ailleurs_1.png";
 import ailleurs_2 from "../public/img/ailleurs_2.png";
 import ailleurs_3 from "../public/img/ailleurs_3.png";
-import ailleurs_Y from "../public/img/ailleurs_Y.png";
-import clairenou_Y from "../public/img/clairenou_Y.png";
+import ailleurs_1_carre from "../public/img/ailleurs_1_carre.png";
+import ailleurs_2_carre from "../public/img/ailleurs_2_carre.png";
+import ailleurs_3_carre from "../public/img/ailleurs_3_carre.png";
+import ailleurs_C from "../public/img/ailleurs_C.png";
+import clairenou_C from "../public/img/clairenou_C.png";
 import clairenou_1 from "../public/img/clairenou_1.png";
 import clairenou_2 from "../public/img/clairenou_2.png";
 import clairenou_3 from "../public/img/clairenou_3.png";
-import wheel_Y from "../public/img/wheel_Y.png";
+import clairenou_1_carre from "../public/img/clairenou_1_carre.png";
+import clairenou_2_carre from "../public/img/clairenou_2_carre.png";
+import clairenou_3_carre from "../public/img/clairenou_3_carre.png";
+import wheel_C from "../public/img/wheel_C.png";
 import wheel_1 from "../public/img/wheel_1.png";
 import wheel_2 from "../public/img/wheel_2.png";
 import wheel_3 from "../public/img/wheel_3.png";
+import wheel_1_carre from "../public/img/wheel_1_carre.png";
+import wheel_2_carre from "../public/img/wheel_2_carre.png";
+import wheel_3_carre from "../public/img/wheel_3_carre.png";
+import portfolio_C from "../public/img/portfolio_C.png";
+import portfolio_1 from "../public/img/portfolio_1.png";
+import portfolio_2 from "../public/img/portfolio_2.png";
+import portfolio_3 from "../public/img/portfolio_3.png";
+import portfolio_1_carre from "../public/img/portfolio_1_carre.png";
+import portfolio_2_carre from "../public/img/portfolio_2_carre.png";
+import portfolio_3_carre from "../public/img/portfolio_3_carre.png";
 
 export type Project = {
     dev: boolean;
@@ -56,7 +72,7 @@ export const PROJECTS_EN: Project[] = [
             "Extracts of several movie shots I worked on between 2016 and 2018.",
         ],
         name: "demo2018",
-        img: [demo2018],
+        img: [demo2018_O],
         imgC: [demo2018_16_9],
         srcVideo:
             "https://player.vimeo.com/video/431456339?portrait=0&title=0&byline=0",
@@ -71,7 +87,7 @@ export const PROJECTS_EN: Project[] = [
             "Extracts of several movie shots I worked on between 2012 and 2016.",
         ],
         name: "demo2016",
-        img: [demo2016],
+        img: [demo2016_O],
         imgC: [demo2016_16_9],
         srcVideo:
             "https://player.vimeo.com/video/194044173?portrait=0&title=0&byline=0",
@@ -80,15 +96,14 @@ export const PROJECTS_EN: Project[] = [
         dev: false,
         id: 3,
         title: "My Garden Map",
-        subtitle:
-            "Animated explainer video to promote 'My Garden Map' web app.",
+        subtitle: "Animated explainer video to promote 'My Garden Map'",
         desc: [
             "My role : design, storyboard, animation (After Effects).",
             "The client : script",
             "Fabrice Sarcy : sound design",
         ],
         name: "gardenmap",
-        img: [gardenMap],
+        img: [gardenMap_O],
         imgC: [gardenMap_16_9],
         srcVideo:
             "https://player.vimeo.com/video/217581808?portrait=0&title=0&byline=0",
@@ -104,7 +119,7 @@ export const PROJECTS_EN: Project[] = [
             "The client : script, storyboard, design, sound",
         ],
         name: "seat",
-        img: [seat],
+        img: [seat_O],
         imgC: [seat_16_9],
         srcVideo:
             "https://player.vimeo.com/video/191660872?portrait=0&title=0&byline=0",
@@ -113,14 +128,14 @@ export const PROJECTS_EN: Project[] = [
         dev: false,
         id: 5,
         title: "Réseau des Amap Idf",
-        subtitle: "Corporate video for 'Réseau des Amaps d'Ile-de-France'",
+        subtitle: "Corporate video for 'Réseau des Amaps IdF'",
         desc: [
             "My role : photo shooting, design (illustrator), storyboard, animation and compositing (after effects)",
             "The client : script",
             "Fabrice Sarcy : sound design",
         ],
         name: "amap",
-        img: [amap],
+        img: [amap_O],
         imgC: [amap_16_9],
         srcVideo:
             "https://player.vimeo.com/video/190995104?portrait=0&title=0&byline=0",
@@ -134,7 +149,7 @@ export const PROJECTS_EN: Project[] = [
             "Having some fun with After Effects plug-ins : DUIK (character's rigging tool), Puppet Pin Tool, Newton... ",
         ],
         name: "walkcycle",
-        img: [walkcycle],
+        img: [walkcycle_O],
         imgC: [walkcycle_16_9],
         srcVideo:
             "https://player.vimeo.com/video/198272473?portrait=0&title=0&byline=0",
@@ -150,8 +165,15 @@ export const PROJECTS_EN: Project[] = [
             "You can also consult thematic maps: security, family life and environment.",
         ],
         name: "ailleurs",
-        img: [ailleurs_Y],
-        imgC: [ailleurs_1, ailleurs_2, ailleurs_3],
+        img: [ailleurs_C],
+        imgC: [
+            ailleurs_1_carre,
+            ailleurs_2_carre,
+            ailleurs_3_carre,
+            ailleurs_1,
+            ailleurs_2,
+            ailleurs_3,
+        ],
         technical_sheet: [
             "To carry out this project, I started by collecting data (data.gouv.fr, INSEE...) that I gathered in a MongoDB database.",
             "By performing many manipulations and joins (aggregations), I was able to export the final data to a JSON file.",
@@ -174,15 +196,22 @@ export const PROJECTS_EN: Project[] = [
     {
         dev: true,
         id: 8,
-        title: "Wheel",
+        title: "Lucky Wheel",
         subtitle: "A random wheel in pure javascript",
         desc: [
             "A wheel is split into as many fractions as there are names given by the user within a limit of 10.",
             "Each wheel fraction has its own random color. As names are designated by the wheel, this last updates its number of fraction and the ranking is displayed.",
         ],
         name: "wheel",
-        img: [wheel_Y],
-        imgC: [wheel_1, wheel_2, wheel_3],
+        img: [wheel_C],
+        imgC: [
+            wheel_1_carre,
+            wheel_2_carre,
+            wheel_3_carre,
+            wheel_1,
+            wheel_2,
+            wheel_3,
+        ],
         technical_sheet: [
             "For this project I used the Canvas object of Javascript and I created classes and Javascript modules.",
         ],
@@ -195,16 +224,22 @@ export const PROJECTS_EN: Project[] = [
         dev: true,
         id: 9,
         title: "Clairenou",
-        subtitle:
-            "A showcase site and a backoffice application for Claire Renou",
+        subtitle: "A showcase site and a backoffice application",
         desc: [
             "Claire Renou is a safety assessor of cosmetics.",
             "She asked me for a showcase site to present her activity but also so that its visitors can book training sessions that she runs herself.",
             "She also needs a tool to automate the creation of EU Product Information File for her clients, which requires a complex database.",
         ],
         name: "clairenou",
-        img: [clairenou_Y],
-        imgC: [clairenou_1, clairenou_2, clairenou_3],
+        img: [clairenou_C],
+        imgC: [
+            clairenou_1_carre,
+            clairenou_2_carre,
+            clairenou_3_carre,
+            clairenou_1,
+            clairenou_2,
+            clairenou_3,
+        ],
         technical_sheet: [
             "I started by designing the relational database, with the Merise method, necessary for Claire's backoffice tool.",
             "A first version of the backoffice project was produced with PHP without a framework. Now I have chosen to switch everything to Node, Express and Typescript.",
@@ -224,6 +259,43 @@ export const PROJECTS_EN: Project[] = [
         preview: false,
         code: false,
     },
+    {
+        dev: true,
+        id: 10,
+        title: "My Portfolio",
+        subtitle: "My portfolio website you're already looking at",
+        desc: [
+            "I made my portfolio with NextJs framework based on React.",
+            "I designed it myself and tried to be UX friendly as possible",
+        ],
+        name: "portfolio",
+        img: [portfolio_C],
+        imgC: [
+            portfolio_1_carre,
+            portfolio_2_carre,
+            portfolio_3_carre,
+            portfolio_1,
+            portfolio_2,
+            portfolio_3,
+        ],
+        technical_sheet: [
+            "I started to sketch a layout on figma.",
+            "I chose to work with Tailwind css framework for its flexibility.",
+            "I made two versions : french and english.",
+        ],
+        technologies: [
+            "NextJS",
+            "Typecript",
+            "Javascript",
+            "HTML5",
+            "CSS",
+            "Sass",
+            "Tailwind",
+        ],
+        preview: false,
+        code: true,
+        github: "https://github.com/Fidomi/portfolio-next",
+    },
 ];
 
 export const PROJECTS_FR: Project[] = [
@@ -237,7 +309,7 @@ export const PROJECTS_FR: Project[] = [
             "Extraits de plusieurs plans auxquels j'ai participé en tant que compeuse ou motion designer entre 2016 et 2018",
         ],
         name: "demo2018",
-        img: [demo2018],
+        img: [demo2018_O],
         imgC: [demo2018_16_9],
         srcVideo:
             "https://player.vimeo.com/video/431456339?portrait=0&title=0&byline=0",
@@ -252,7 +324,7 @@ export const PROJECTS_FR: Project[] = [
             "Extraits de plusieurs plans auxquels j'ai participés en tant que compeuse ou motion designer entre 2012 et 2016.",
         ],
         name: "demo2016",
-        img: [demo2016],
+        img: [demo2016_O],
         imgC: [demo2016_16_9],
         srcVideo:
             "https://player.vimeo.com/video/194044173?portrait=0&title=0&byline=0",
@@ -261,14 +333,14 @@ export const PROJECTS_FR: Project[] = [
         dev: false,
         id: 3,
         title: "My Garden Map",
-        subtitle: "Vidéo explicative du fonctionnement de l'app My Garden Map",
+        subtitle: "Vidéo explicative de l'app 'My Garden Map'",
         desc: [
             "Ma partie : design, storyboard, animation (After Effects).",
             "Le client : scénario",
             "Fabrice Sarcy : sound design",
         ],
         name: "gardenmap",
-        img: [gardenMap],
+        img: [gardenMap_O],
         imgC: [gardenMap_16_9],
         srcVideo:
             "https://player.vimeo.com/video/217581808?portrait=0&title=0&byline=0",
@@ -277,14 +349,13 @@ export const PROJECTS_FR: Project[] = [
         dev: false,
         id: 4,
         title: "Seat",
-        subtitle:
-            "Extraits d'une série de vidéo réalisées pour Seat expliquant leur stratégie digitale",
+        subtitle: "Extraits d'une série de vidéo réalisées pour Seat",
         desc: [
             "Ma mission : animation sur After Effects",
             "Le client : scénario, storyboard, design",
         ],
         name: "seat",
-        img: [seat],
+        img: [seat_O],
         imgC: [seat_16_9],
         srcVideo:
             "https://player.vimeo.com/video/191660872?portrait=0&title=0&byline=0",
@@ -293,14 +364,14 @@ export const PROJECTS_FR: Project[] = [
         dev: false,
         id: 5,
         title: "Réseau des Amap Idf",
-        subtitle: "Présentation du 'Réseau des Amaps d'Ile-de-France'",
+        subtitle: "Présentation du 'Réseau des Amaps IdF'",
         desc: [
             "Ma mission : prises de vues, design (illustrator), storyboard, animation et compositing (after effects)",
             "Le client : scénario",
             "Fabrice Sarcy : sound design",
         ],
         name: "amap",
-        img: [amap],
+        img: [amap_O],
         imgC: [amap_16_9],
         srcVideo:
             "https://player.vimeo.com/video/190995104?portrait=0&title=0&byline=0",
@@ -314,7 +385,7 @@ export const PROJECTS_FR: Project[] = [
             "Quelques essais avec des plug-ins d'After : DUIK (outil de rigging 2D de personnages), Puppet Pin Tool, Newton... ",
         ],
         name: "walkcycle",
-        img: [walkcycle],
+        img: [walkcycle_O],
         imgC: [walkcycle_16_9],
         srcVideo:
             "https://player.vimeo.com/video/198272473?portrait=0&title=0&byline=0",
@@ -330,8 +401,15 @@ export const PROJECTS_FR: Project[] = [
             "On peut aussi consulter des cartes thématiques : sécurité, vie de famille et environnement.",
         ],
         name: "ailleurs",
-        img: [ailleurs_Y],
-        imgC: [ailleurs_1, ailleurs_2, ailleurs_3],
+        img: [ailleurs_C],
+        imgC: [
+            ailleurs_1_carre,
+            ailleurs_2_carre,
+            ailleurs_3_carre,
+            ailleurs_1,
+            ailleurs_2,
+            ailleurs_3,
+        ],
         technical_sheet: [
             "Pour réaliser ce projet, j'ai commencé par collecter des données (data.gouv.fr, INSEE...) que j'ai réunies dans une base de données MongoDB.",
             "En réalisant de nombreuses manipulations et jointures (aggregations), j'ai pu exporter les données finales dans un fichier JSON.",
@@ -354,15 +432,22 @@ export const PROJECTS_FR: Project[] = [
     {
         dev: true,
         id: 8,
-        title: "Wheel",
+        title: "La Roue du Hasard",
         subtitle: "Une roue de tirage au sort en pur javascript",
         desc: [
-            "Une roue de tirage est fractionnée en autant de noms que l'utilisateur veut entrer dans une limite de 10.",
+            "L'utilisateur entre les noms à tirer au sort. La roue compte une fraction par nom.",
             "Chaque fraction de roue a sa propre couleur aléatoire. Au fur et à mesure que les noms sont désignés par la roue, cette dernière se met à jour et le palmarès s'affiche.",
         ],
         name: "wheel",
-        img: [wheel_Y],
-        imgC: [wheel_1, wheel_2, wheel_3],
+        img: [wheel_C],
+        imgC: [
+            wheel_1_carre,
+            wheel_2_carre,
+            wheel_3_carre,
+            wheel_1,
+            wheel_2,
+            wheel_3,
+        ],
         technical_sheet: [
             "Pour réaliser ce projet j'ai utilisé l'objet Canvas de Javascript et j'ai créé des classes et des modules Javascript.",
         ],
@@ -375,16 +460,22 @@ export const PROJECTS_FR: Project[] = [
         dev: true,
         id: 9,
         title: "Clairenou",
-        subtitle:
-            "Un site vitrine et une application backoffice pour Claire Renou",
+        subtitle: "Un site vitrine et une application backoffice",
         desc: [
             "Claire Renou est évaluatrice de la toxicité des cosmétiques.",
             "Elle m'a commandé un site vitrine pour présenter son activité mais aussi pour que ses visiteurs puissent réserver des sessions de formation à l'évaluation qu'elle anime elle-même.",
             "Elle a aussi besoin d'un outil pour automatiser la création des dossiers de toxicité de ses clients qui requiert une base de données complexe.",
         ],
         name: "clairenou",
-        img: [clairenou_Y],
-        imgC: [clairenou_1, clairenou_2, clairenou_3],
+        img: [clairenou_C],
+        imgC: [
+            clairenou_1_carre,
+            clairenou_2_carre,
+            clairenou_3_carre,
+            clairenou_1,
+            clairenou_2,
+            clairenou_3,
+        ],
         technical_sheet: [
             "J'ai commencé par designer avec la méthode Merise la base de données relationnelles nécessaire à l'outil backoffice de Claire.",
             "Une première version du projet backoffice a été réalisée avec PHP sans framework. Maintenant j'ai choisi de tout basculer sur Node, Express et Typescript.",
@@ -403,5 +494,42 @@ export const PROJECTS_FR: Project[] = [
         ],
         preview: false,
         code: false,
+    },
+    {
+        dev: true,
+        id: 10,
+        title: "Mon Portfolio",
+        subtitle: "Le site portfolio sur lequel vous êtes actuellement.",
+        desc: [
+            "J'ai façonné ce portfolio avec le framework Next basé sur React.",
+            "Mon objectif est de présenter mes compétences en développement et en VFX mais avec une dominante dév.",
+        ],
+        name: "portfolio",
+        img: [portfolio_C],
+        imgC: [
+            portfolio_1_carre,
+            portfolio_2_carre,
+            portfolio_3_carre,
+            portfolio_1,
+            portfolio_2,
+            portfolio_3,
+        ],
+        technical_sheet: [
+            "J'ai d'abord esquissé une mise en page sur figma.",
+            "J'ai choisi de travailler avec le framework css Tailwind pour sa souplesse",
+            "J'ai fait deux versions : français et anglais.",
+        ],
+        technologies: [
+            "NextJS",
+            "Typecript",
+            "Javascript",
+            "HTML5",
+            "CSS",
+            "Sass",
+            "Tailwind",
+        ],
+        preview: false,
+        code: true,
+        github: "https://github.com/Fidomi/portfolio-next",
     },
 ];
