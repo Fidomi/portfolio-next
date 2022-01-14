a0aa329af3b0b1d65c3cd31cf37584bb9d491427
Fidomi <sidonie.moulart@gmail.com>
2022-01-14 10:44:38 +0100

    Add html lang en

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
         deviceSizes: [640, 768, 1024, 1280, 1536],
         imageSizes: [16, 32, 48, 64, 96],
     },
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
 };
