dfb8fc8f216fc61954de3a382d8df110c0bb4bda
Fidomi <sidonie.moulart@gmail.com>
2022-01-27 00:00:09 +0100

    Change grant

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Layout } from "../components/Layout";
 import Map from "../components/Map/Map";
+import { ShowedProjectProvider } from "../utils/context";
+import { LanguageProvider } from "../utils/languageContext";
 
 export default function Home() {
     return (
-        <Layout>
-            <Map />
-        </Layout>
+        <LanguageProvider>
+            <ShowedProjectProvider>
+                <Layout>
+                    <Map />
+                </Layout>
+            </ShowedProjectProvider>
+        </LanguageProvider>
     );
 }
